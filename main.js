7dff32def7cb252606edd2c70ddb32449670ceb3
Abdullahi <abdullahi45230@student.simad.edu.so>
2026-02-05 16:42:20 +0300

    new

--- main.js
+++ main.js
@@ -5,30 +5,55 @@ const password = document.querySelector("#password");
 const confimpassword = document.querySelector("#confimpassword");
 const form = document.querySelector("#form");
 
+const showError = (input,message)=>{
+let parentElement= input.parentElement;
+parentElement.classList = 'form-control error';
+const small = parentElement.querySelector("small")
+const successIcon=parentElement.querySelectorAll("i")[0];
+const errorIcon=parentElement.querySelectorAll("i")[1];
+errorIcon.style.visibility ='visible';
+successIcon.style.visibility ='hidden';
+small.innerText = message;
+}
+
+const showSuccess = (input)=>{
+let parentElement= input.parentElement;
+parentElement.classList = 'form-control error';
+// const small = parentElement.querySelector("small")
+const successIcon=parentElement.querySelectorAll("i")[0];
+const errorIcon=parentElement.querySelectorAll("i")[1];
+errorIcon.style.visibility ='hidden';
+successIcon.style.visibility ='visible';
+// small.innerText = message;
+}
+const checkEmpty=(elements)=>{
+elements.forEach(element => {
+    if(element.value ===''){
+        showError(element,'input required')
+    }else{
+       showSuccess(element);
+    } 
+});
+}
+
 form.addEventListener('submit',(event)=>{
 event.preventDefault();
-
-if (username.value === ''){
-    username.parentElement.classList = 'form-control error';
-}
-
-
-if (Email.value === ''){
-    Email.parentElement.classList = 'form-control error';
-}
-
-
-if (password.value === ''){
-    password.parentElement.classList = 'form-control error';
-}
-
-
-if (confimpassword.value === ''){
-    confimpassword.parentElement.classList = 'form-control error';
-}
+checkEmpty([username, Email, password,confimpassword]);
 
 });
 
+// if (username.value === ''){
+//     username.parentElement.classList = 'form-control error';
+// }
+// if (Email.value === ''){
+//     Email.parentElement.classList = 'form-control error';
+// }
+// if (password.value === ''){
+//     password.parentElement.classList = 'form-control error';
+// }
+// if (confimpassword.value === ''){
+//     confimpassword.parentElement.classList = 'form-control error';
+// }
 
 
 
